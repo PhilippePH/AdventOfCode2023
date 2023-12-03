865a7a3dce6753d53e928d68f0c9f556c71ce4c2
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-12-03 14:37:10 -0500

    Day 3 Part 2 Solved

--- day3/solution.py
+++ day3/solution.py
@@ -59,6 +59,34 @@ def addNumbers(puzzleInput, lineNumber, columnNumber):
 
 	return total
 
+
+def addGearRatios(puzzleInput, lineNumber, columnNumber):
+	number1 = 0
+	number2 = 0
+	visited = []
+
+	for lineOffset in [-1, 0, 1]:
+		for columnOffset in [-1, 0, 1]:
+			x = lineNumber + lineOffset
+			y = columnNumber + columnOffset
+			coordinate = str(x)+str(y)
+
+			if coordinate not in visited:
+				if puzzleInput[x][y].isdigit():
+					number, coordinates = createNumber(puzzleInput, x, y)
+					if number1 == 0:
+						number1 = number
+					elif number2 == 0:
+						number2 = number
+					else :
+						sys.exit("Hmmm I found to many numbers..")
+
+					for coordinateSet in coordinates:
+						visited.append(coordinateSet)
+
+
+	return (number1 * number2)
+
 def solveProblemOne(puzzleInput):
 	total = 0
 
@@ -68,21 +96,32 @@ def solveProblemOne(puzzleInput):
 				total += addNumbers(puzzleInput, lineNumber, columnNumber)
 	return total
 
+def solveProblemTwo(puzzleInput):
+	gearRatio = 0
+
+	for lineNumber in range(len(puzzleInput)):
+		for columnNumber in range(len(puzzleInput[lineNumber])):
+			if puzzleInput[lineNumber][columnNumber] == "*":
+				gearRatio += addGearRatios(puzzleInput, lineNumber, columnNumber)
+
+	return gearRatio
+
 def main():
-	testInput = readFile("test1.txt")
-	testResult = solveProblemOne(testInput)
-	print(testResult)
-
-	print("------\n")
-
-	#testInput = readFile("day2_test2.txt")
-	#testResult = solveProblemTwo(testInput)
+	#testInput = readFile("test1.txt")
+	#testResult = solveProblemOne(testInput)
 	#print(testResult)
 
 	print("------\n")
 
-	input = readFile("input.txt");
-	result = solveProblemOne(input)
+	testInput = readFile("test1.txt")
+	testResult = solveProblemTwo(testInput)
+	print(testResult)
+
+	print("------\n")
+
+	puzzleInput = readFile("input.txt")
+	#result = solveProblemOne(puzzleInput)
+	result = solveProblemTwo(puzzleInput)
 	print(result)
 
 if __name__ == "__main__":
